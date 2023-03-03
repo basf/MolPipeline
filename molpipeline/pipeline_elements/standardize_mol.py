f836b037dffae13f2455adcfdcfcb0b5f1baeb9e
feldmacw <christian-wolfgang.feldmann@basf.com>
2023-03-03 13:34:17 +0100

    Add RemoveChargePipe

--- molpipeline/pipeline_elements/standardize_mol.py
+++ molpipeline/pipeline_elements/standardize_mol.py
@@ -8,6 +8,17 @@ from molpipeline.pipeline_elements.abstract_pipeline_elements import Mol2MolPipe
 from molpipeline.utils.molpipe_types import OptionalMol
 
 
+class RemoveChargePipe(_Mol2MolPipe):
+    def __init__(self, name: str = "RemoveChargePipe") -> None:
+        super(RemoveChargePipe, self).__init__(name)
+
+    def fit(self, input_values: Any) -> None:
+        pass
+
+    def transform_single(self, mol: Chem.Mol) -> OptionalMol:
+        return rdMolStandardize.ChargeParent(mol)
+
+
 class MetalDisconnectorPipe(_Mol2MolPipe):
     def __init__(self, name: str = "MetalDisconnectorPipe") -> None:
         super(MetalDisconnectorPipe, self).__init__(name)
