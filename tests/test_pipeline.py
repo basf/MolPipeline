309246d89eb3ca1c76bd0a151e7141332c5f670c
feldmacw <christian-wolfgang.feldmann@basf.com>
2023-03-03 13:35:50 +0100

    Updating Unittest

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -8,25 +8,23 @@ from rdkit.Chem.AllChem import GetMorganFingerprintAsBitVect
 from molpipeline.pipeline import MolPipeline
 from molpipeline.pipeline_elements.any2mol import Smiles2MolPipe
 from molpipeline.pipeline_elements.mol2fingerprint import Mol2FoldedMorganFingerprintPipe
-from molpipeline.pipeline_elements.mol2mol import ReactionPipe
+from molpipeline.pipeline_elements.standardize_mol import (
+    RemoveChargePipe,
+    MetalDisconnectorPipe,
+    SaltRemoverPipe
+
+)
+from molpipeline.pipeline_elements.mol2any import Mol2SmilesPipe
 from molpipeline.utils.matrices import are_equal
+
+from utils.fingerprints import make_sparse_fp
+
 
 TEST_SMILES = ["CC", "CCO", "COC", "CCCCC", "CCC(-O)O", "CCCN"]
 CONTAINS_OX = [0, 1, 1, 0, 1, 0]
 FP_RADIUS = 2
 FP_SIZE = 2048
-
-
-def make_sparse_fp(smiles_list: list[str]) -> sparse.csr_matrix:
-    vector_list = []
-    for smiles in smiles_list:
-        mol = Chem.MolFromSmiles(smiles)
-        vector = GetMorganFingerprintAsBitVect(mol, radius=FP_RADIUS, nBits=FP_SIZE)
-        vector_list.append(sparse.csr_matrix(vector))
-    return sparse.vstack(vector_list)
-
-
-EXPECTED_OUTPUT = make_sparse_fp(TEST_SMILES)
+EXPECTED_OUTPUT = make_sparse_fp(TEST_SMILES, FP_RADIUS, FP_SIZE)
 
 
 class PipelineTest(unittest.TestCase):
@@ -59,8 +57,39 @@ class PipelineTest(unittest.TestCase):
             self.assertEqual(pred_val, true_val)
 
     def test_sliciing(self) -> None:
-        pipeline_element_list = [Smiles2MolPipe()]
+        pipeline_element_list = [
+            Smiles2MolPipe(), MetalDisconnectorPipe(), SaltRemoverPipe(), Mol2SmilesPipe()]
+        m_pipeline = MolPipeline(pipeline_element_list)
 
+        first_half = m_pipeline[:2]
+        self.assertTrue(first_half.pipeline_elements[0] is pipeline_element_list[0])
+        self.assertTrue(first_half.pipeline_elements[1] is pipeline_element_list[1])
+
+        second_half = m_pipeline[2:]
+        self.assertTrue(second_half.pipeline_elements[0] is pipeline_element_list[2])
+        self.assertTrue(second_half.pipeline_elements[1] is pipeline_element_list[3])
+
+        concatenated_pipeline = first_half + second_half
+        for concat_element, original_element in zip(concatenated_pipeline.pipeline_elements, pipeline_element_list):
+            self.assertTrue(concat_element is original_element)
+
+    def test_salt_removal(self) -> None:
+        smiles_with_salt_list = ["CCO-[Na]", "CCC(=O)[O-].[Li+]", "CCC(=O)-O-[K]"]
+        smiles_without_salt_list = ["CCO", "CCC(=O)O", "CCC(=O)O"]
+
+        salt_remover_pipeline = MolPipeline(
+            [
+                Smiles2MolPipe(),
+                MetalDisconnectorPipe(),
+                SaltRemoverPipe(),
+                RemoveChargePipe(),
+                Mol2SmilesPipe()
+
+            ]
+        )
+        generated_smiles = salt_remover_pipeline.transform(smiles_with_salt_list)
+        for generated_smiles, smiles_without_salt in zip(generated_smiles, smiles_without_salt_list):
+            self.assertEqual(generated_smiles, smiles_without_salt)
 
 if __name__ == "__main__":
     unittest.main()
