16a89d72d5ddb522364c3df9e63625babc930110
feldmacw <christian-wolfgang.feldmann@basf.com>
2023-03-06 22:36:13 +0100

    black...

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     author="Christian W. Feldmann",
     author_email="christian-wolfgang.feldmann@basf.com",
     description="",
-    requires=["scikit-learn", "rdkit", "numpy", "scipy"]
+    requires=["scikit-learn", "rdkit", "numpy", "scipy"],
 )
