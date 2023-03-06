9143524bfab7a56e444da211ae8ff472287ece11
feldmacw <christian-wolfgang.feldmann@basf.com>
2023-03-06 23:03:47 +0100

    Refining test

--- tests/test_elements/test_mol2concatenated.py
+++ tests/test_elements/test_mol2concatenated.py
@@ -1,6 +1,7 @@
 import numpy as np
 import unittest
 
+from rdkit import Chem
 
 from molpipeline.pipeline import MolPipeline
 from molpipeline.pipeline_elements.mol2any.mol2rdkit_phys_chem import MolToRDKitPhysChem
@@ -37,12 +38,21 @@ class TestConcatenatedFingerprint(unittest.TestCase):
         ]
         output = pipeline.fit_transform(smiles)
         output2 = pipeline.transform(smiles)
+
+        mol_list = [Chem.MolFromSmiles(smi) for smi in smiles]
+        output3 = np.hstack(
+            [
+                property_element.transform(mol_list),
+                morgan_element.transform(mol_list).toarray(),
+            ]
+        )
         expected_shape = (
             len(smiles),
             (property_element.n_features + morgan_element.n_bits),
         )
         self.assertTrue(output.shape == expected_shape)
         self.assertTrue(np.abs(output - output2).max() < 0.00001)
+        self.assertTrue(np.abs(output - output3).max() < 0.00001)
 
 
 if __name__ == "__main__":
