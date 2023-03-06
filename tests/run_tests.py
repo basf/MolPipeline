bc7e3875781058a02cd1814bc230e5996f9d5be1
feldmacw <christian-wolfgang.feldmann@basf.com>
2023-03-06 22:55:27 +0100

    Fixing Unittest

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -6,6 +6,3 @@ import unittest
 if __name__ == "__main__":
     pipeline_test = unittest.TestLoader().discover(".")
     unittest.TextTestRunner(verbosity=2).run(pipeline_test)
-
-    element_tests = unittest.TestLoader().discover("./test_elements/")
-    unittest.TextTestRunner(verbosity=2).run(element_tests)
