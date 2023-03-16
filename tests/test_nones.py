8bb349f119b3ec92cfbde6b030697146730c03c0
feldmacw <christian-wolfgang.feldmann@basf.com>
2023-03-16 13:16:50 +0100

    Pleasing linter

--- tests/test_nones.py
+++ tests/test_nones.py
@@ -1,3 +1,5 @@
+"""Unittest for handling Nones."""
+
 import unittest
 
 from molpipeline.pipeline import MolPipeline
@@ -5,18 +7,20 @@ from molpipeline.pipeline_elements.any2mol.smiles2mol import SmilesToMolPipeline
 from molpipeline.pipeline_elements.mol2any.mol2smiles import MolToSmilesPipelineElement
 
 
-TEST_SMILES = ["NCCCO","abc"]
-EXPECTED_OUTPUT = ["NCCCO",None]
+TEST_SMILES = ["NCCCO", "abc"]
+EXPECTED_OUTPUT = ["NCCCO", None]
+
 
 class NoneTest(unittest.TestCase):
+    """Unittest for None Handling."""
     def test_easy_none_molpipeline(self) -> None:
-        pipeline = MolPipeline([
-            SmilesToMolPipelineElement(),
-            MolToSmilesPipelineElement(),
-            ])
+        """Assert that invalid smiles are transformed to None."""
+        pipeline = MolPipeline(
+            [
+                SmilesToMolPipelineElement(),
+                MolToSmilesPipelineElement(),
+            ]
+        )
         out = pipeline.transform(TEST_SMILES)
         for pred_val, true_val in zip(out, EXPECTED_OUTPUT):
             self.assertEqual(pred_val, true_val)
-
-
-
