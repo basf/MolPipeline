979c55b1ade4592508d273727c697f1f405be5e5
Frederik Sandfort <frederik.sandfort@basf.com>
2023-05-03 22:14:06 +0200

    make uncharger saltremover picklable

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="molpipeline",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(),
     url="",
     license="",
