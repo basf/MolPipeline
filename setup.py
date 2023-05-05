fed216768124b9d9418da5f0a71334eda26585ad
Christian W. Feldmann <christian-wolfgang.feldmann@basf.com>
2023-05-05 13:48:52 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="molpipeline",
-    version="0.3.4",
+    version="0.3.5",
     packages=find_packages(),
     url="",
     license="",
