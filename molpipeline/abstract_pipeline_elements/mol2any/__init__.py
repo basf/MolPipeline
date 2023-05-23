2e547febcf713293c07a3e0e5553b3bf5c377e91
feldmacw <christian-wolfgang.feldmann@basf.com>
2023-05-23 14:43:38 +0200

    Adding reference to init.py

--- molpipeline/abstract_pipeline_elements/mol2any/__init__.py
+++ molpipeline/abstract_pipeline_elements/mol2any/__init__.py
@@ -1,1 +1,10 @@
 """Init."""
+from molpipeline.abstract_pipeline_elements.mol2any.mol2bitvector import MolToFingerprintPipelineElement
+from molpipeline.abstract_pipeline_elements.mol2any.mol2floatvector import MolToDescriptorPipelineElement
+from molpipeline.abstract_pipeline_elements.mol2any.mol2string import MolToStringPipelineElement
+
+__all__ = (
+    "MolToFingerprintPipelineElement",
+    "MolToDescriptorPipelineElement",
+    "MolToStringPipelineElement",
+)
