7e971ad2dea0513a2ade5cc04d10397f73add4e1
feldmacw <christian-wolfgang.feldmann@basf.com>
2023-07-17 20:22:42 +0200

    pylint

--- molpipeline/post_prediction_pipeline/__init__.py
+++ molpipeline/post_prediction_pipeline/__init__.py
@@ -1,2 +1,11 @@
-from molpipeline.post_prediction_pipeline.meta_cluster import ClusterMerger
+"""Initialize post_prediction_pipeline module.
+
+This module contains classes for post prediction processing of predictions.
+"""
+from molpipeline.post_prediction_pipeline.meta_cluster import ClusterMerging
 from molpipeline.post_prediction_pipeline.post_prediction_pipeline import PostPredictionPipeline
+
+__all__ = [
+    "PostPredictionPipeline",
+    "ClusterMerging",
+]
