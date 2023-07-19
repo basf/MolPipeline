0d51aeb9ec983195233664830b1bcc638060032e
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2023-07-19 10:09:35 +0200

    Bumping version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt", mode="r", encoding="utf-8") as requirement_file:
 
 setup(
     name="molpipeline",
-    version="0.3.6",
+    version="0.3.7",
     packages=find_packages(),
     url="",
     license="",
