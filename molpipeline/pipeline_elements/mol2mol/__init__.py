f2d7e6bbd5265299662edb8946a8e474f40b19e7
feldmacw <christian-wolfgang.feldmann@basf.com>
2023-09-04 14:01:19 +0200

    Rename UniqueFragmentsByXXX to DeduplicateFragmentsByXXX

--- molpipeline/pipeline_elements/mol2mol/__init__.py
+++ molpipeline/pipeline_elements/mol2mol/__init__.py
@@ -6,14 +6,20 @@ from molpipeline.pipeline_elements.mol2mol.mol2mol_reaction import (
 from molpipeline.pipeline_elements.mol2mol.mol2mol_standardization import (
     MetalDisconnectorPipelineElement,
     ChargeParentPipelineElement,
+    RemoveStereoInformationPipelineElement,
     SaltRemoverPipelineElement,
     UnchargePipelineElement,
+    DeduplicateFragmentsBySmilesElement,
+    DeduplicateFragmentsByInchiElement,
 )
 
 __all__ = (
+    "ChargeParentPipelineElement",
     "MolToMolReactionPipelineElement",
     "MetalDisconnectorPipelineElement",
-    "ChargeParentPipelineElement",
+    "RemoveStereoInformationPipelineElement",
     "SaltRemoverPipelineElement",
     "UnchargePipelineElement",
+    "DeduplicateFragmentsBySmilesElement",
+    "DeduplicateFragmentsByInchiElement",
 )
