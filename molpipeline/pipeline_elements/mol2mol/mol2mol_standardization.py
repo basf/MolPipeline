6d0212a8c48756623a7c0d0aeb638ee0e4363f29
feldmacw <christian-wolfgang.feldmann@basf.com>
2023-09-04 10:34:06 +0200

    Add Pipeline Elements for uniquifying fragments in the molecule.

--- molpipeline/pipeline_elements/mol2mol/mol2mol_standardization.py
+++ molpipeline/pipeline_elements/mol2mol/mol2mol_standardization.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 from typing import Optional
 
+from rdkit import Chem
 from rdkit.Chem import Mol as RDKitMol  # type: ignore[import]
 from rdkit.Chem import rdmolops, SanitizeMol
 from rdkit.Chem import SaltRemover as rdkit_SaltRemover
@@ -10,6 +11,7 @@ from rdkit.Chem.MolStandardize import rdMolStandardize
 
 from molpipeline.abstract_pipeline_elements.core import (
     MolToMolPipelineElement as _MolToMolPipelineElement,
+    InvalidInstance,
 )
 from molpipeline.utils.molpipeline_types import OptionalMol
 
@@ -178,7 +180,7 @@ class UnchargePipelineElement(_MolToMolPipelineElement):
 
     def __init__(
         self,
-        name: str = "UnchargePipe",
+        name: str = "UnchargePipelineElement",
         n_jobs: int = 1,
         uuid: Optional[str] = None,
     ) -> None:
@@ -199,3 +201,77 @@ class UnchargePipelineElement(_MolToMolPipelineElement):
             Uncharged molecule if possible, else InvalidInstance.
         """
         return rdMolStandardize.Uncharger().uncharge(value)
+
+
+class UniqueFragmentsBySmilesElement(_MolToMolPipelineElement):
+    """MolToMolPipelineElement which removes duplicate fragments from a molecule.
+
+    Duplicates are detected by comparing the SMILES of the fragments.
+    """
+    def __int__(self, name: str = "UniqueFragmentsBySmiles", n_jobs: int = 1, uuid: Optional[str] = None) -> None:
+        """Initialize UniqueFragmentsBySmilesElement."""
+        super().__init__(name=name, n_jobs=n_jobs, uuid=uuid)
+
+    def pretransform_single(self, value: RDKitMol) -> OptionalMol:
+        """Remove duplicate fragments from molecule.
+
+        Parameters
+        ----------
+        value: RDKitMol
+            Molecule to remove duplicate fragments from.
+
+        Returns
+        -------
+        OptionalMol
+            Molecule without duplicate fragments if possible, else InvalidInstance.
+        """
+        fragments = Chem.GetMolFrags(value, asMols=True)
+        fragment_smiles = [Chem.MolToSmiles(fragment) for fragment in fragments]
+        unique_fragment_list = sorted(set(fragment_smiles))
+        unique_fragments = [Chem.MolFromSmiles(smiles) for smiles in unique_fragment_list]
+        if None in unique_fragments:
+            return InvalidInstance(self.uuid, "Could not recreate molecule from SMILES.")
+        if len(unique_fragments) == 1:
+            combined_fragments = unique_fragments[0]
+        else:
+            combined_fragments = Chem.CombineMols(*unique_fragments)
+        for key, value in value.GetPropsAsDict(includeComputed=False).items():
+            combined_fragments.SetProp(key, value)
+        return combined_fragments
+
+
+class UniqueFragmentsByInchiElement(_MolToMolPipelineElement):
+    """MolToMolPipelineElement which removes duplicate fragments from a molecule.
+
+    Duplicates are detected by comparing the InChI of the fragments.
+    """
+    def __int__(self, name: str = "UniqueFragmentsByInchi", n_jobs: int = 1, uuid: Optional[str] = None) -> None:
+        """Initialize UniqueFragmentsByInchiElement."""
+        super().__init__(name=name, n_jobs=n_jobs, uuid=uuid)
+
+    def pretransform_single(self, value: RDKitMol) -> OptionalMol:
+        """Remove duplicate fragments from molecule.
+
+        Parameters
+        ----------
+        value: RDKitMol
+            Molecule to remove duplicate fragments from.
+
+        Returns
+        -------
+        OptionalMol
+            Molecule without duplicate fragments if possible, else InvalidInstance.
+        """
+        fragments = Chem.GetMolFrags(value, asMols=True)
+        fragment_inchis = [Chem.MolToInchi(fragment) for fragment in fragments]
+        unique_fragment_list = sorted(set(fragment_inchis))
+        unique_fragments = [Chem.MolFromInchi(inchi) for inchi in unique_fragment_list]
+        if None in unique_fragments:
+            return InvalidInstance(self.uuid, "Could not recreate molecule from InChI.")
+        if len(unique_fragments) == 1:
+            combined_fragments = unique_fragments[0]
+        else:
+            combined_fragments = Chem.CombineMols(*unique_fragments)
+        for key, value in value.GetPropsAsDict(includeComputed=False).items():
+            combined_fragments.SetProp(key, value)
+        return combined_fragments
--- tests/test_elements/test_mol2mol/test_mol2mol_standardization.py
+++ tests/test_elements/test_mol2mol/test_mol2mol_standardization.py
@@ -4,6 +4,8 @@ from molpipeline.pipeline_elements.any2mol.smiles2mol import SmilesToMolPipeline
 from molpipeline.pipeline_elements.mol2mol.mol2mol_standardization import (
     RemoveStereoInformationPipelineElement,
     MetalDisconnectorPipelineElement,
+    UniqueFragmentsBySmilesElement,
+    UniqueFragmentsByInchiElement,
 )
 from molpipeline.pipeline_elements.mol2any.mol2smiles import MolToSmilesPipelineElement
 
@@ -59,6 +61,62 @@ class MolStandardizationTest(unittest.TestCase):
         # Without additional sanitiziting after disconnecting metals the following would fail with
         # a pre-condition assert from within RDkit.
         self.assertEqual(mols_processed[0].GetRingInfo().NumRings(), 1)
+
+    def test_duplicate_fragment_by_smiles_removal(self) -> None:
+        """Test metal disconnector returns valid molecules containing ring info.
+
+        Returns
+        -------
+        None
+        """
+        duplicate_fragment_smiles_lust = [
+            "CC.CC.C",
+            "c1ccccc1.C1=C-C=C-C=C1",
+        ]
+        expected_unique_fragment_smiles_list = ["C.CC", "c1ccccc1"]
+
+        smi2mol = SmilesToMolPipelineElement()
+        unique_fragments = UniqueFragmentsBySmilesElement()
+        mol2smi = MolToSmilesPipelineElement()
+        pipeline = Pipeline(
+            [
+                ("smi2mol", smi2mol),
+                ("unique_fragments", unique_fragments),
+                ("mol2smi", mol2smi),
+            ]
+        )
+        mols_processed = pipeline.fit_transform(
+            duplicate_fragment_smiles_lust
+        )
+        self.assertEqual(expected_unique_fragment_smiles_list, mols_processed)
+
+    def test_duplicate_fragment_by_inchi_removal(self) -> None:
+        """Test metal disconnector returns valid molecules containing ring info.
+
+        Returns
+        -------
+        None
+        """
+        duplicate_fragment_smiles_lust = [
+            "CC.CC.C",
+            "c1ccccc1.C1=C-C=C-C=C1",
+        ]
+        expected_unique_fragment_smiles_list = ["C.CC", "c1ccccc1"]
+
+        smi2mol = SmilesToMolPipelineElement()
+        unique_fragments = UniqueFragmentsByInchiElement()
+        mol2smi = MolToSmilesPipelineElement()
+        pipeline = Pipeline(
+            [
+                ("smi2mol", smi2mol),
+                ("unique_fragments", unique_fragments),
+                ("mol2smi", mol2smi),
+            ]
+        )
+        mols_processed = pipeline.fit_transform(
+            duplicate_fragment_smiles_lust
+        )
+        self.assertEqual(expected_unique_fragment_smiles_list, mols_processed)
 
 
 if __name__ == "__main__":
