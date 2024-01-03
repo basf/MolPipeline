9554a85c5cf8aa7a1858290231c23bb26a590928
Jochen Sieg <jochen.sieg@basf.com>
2024-01-03 18:17:41 +0100

    sklearn estimator for murcko scaffold clustering
        - Add sklearn estimator class for clustering
          based on murcko scaffolds.
        - Policies are implemented to handle linear
          molecules in the clustering.

---
+++ molpipeline/sklearn_estimators/murcko_scaffold_clustering.py
@@ -0,0 +1,207 @@
+"""Murcko scaffold clustering estimator."""
+from __future__ import annotations
+
+from numbers import Integral
+
+import numpy as np
+import numpy.typing as npt
+from rdkit.Chem import MolFromSmiles
+from sklearn.base import BaseEstimator, ClusterMixin, _fit_context
+from sklearn.preprocessing import FunctionTransformer, OrdinalEncoder
+
+from molpipeline.pipeline import Pipeline
+from molpipeline.pipeline_elements.any2mol import SmilesToMolPipelineElement
+from molpipeline.pipeline_elements.error_handling import ErrorFilter, ErrorReplacer
+from molpipeline.pipeline_elements.mol2any import MolToSmilesPipelineElement
+from molpipeline.pipeline_elements.mol2mol import MurckoScaffoldPipelineElement
+from molpipeline.utils.molpipeline_types import OptionalMol, RDKitMol
+
+try:
+    from typing import Optional, Self
+except ImportError:
+    from typing_extensions import Self
+
+
+class _MurckoScaffoldClusteringHelperPipelineElement(MurckoScaffoldPipelineElement):
+    """MolToMolPipelineElement which yields the Murcko-scaffold of a Molecule and handles linear molecules."""
+
+    # A string representation for linear molecules
+    LINEAR_MOLECULE_DUMMY_SCAFFOLD_MOL: RDKitMol = MolFromSmiles("C")
+
+    def __init__(
+        self,
+        name: str = "_MurckoScaffoldOrLinearPipelineElement",
+        n_jobs: int = 1,
+        uuid: Optional[str] = None,
+    ) -> None:
+        """Initialize MurckoScaffoldPipelineElement.
+
+        Parameters
+        ----------
+        name: str
+            Name of pipeline element.
+        n_jobs: int
+            Number of jobs to use for parallelization.
+        uuid: Optional[str]
+            UUID of pipeline element.
+
+        Returns
+        -------
+        None
+        """
+        super().__init__(name=name, n_jobs=n_jobs, uuid=uuid)
+
+    def pretransform_single(self, value: RDKitMol) -> OptionalMol:
+        """Extract Murco-scaffold of molecule.
+
+        Parameters
+        ----------
+        value: RDKitMol
+            RDKit molecule object which is transformed.
+
+        Returns
+        -------
+        OptionalMol
+            Murco-scaffold of molecule if possible, else InvalidInstance.
+        """
+        scaffold_result: RDKitMol = super().pretransform_single(value)
+        # if no scaffold could be extracted a Molecule with zero atoms is returned. To avoid this entry
+        # being substituted by an InvalidInstance by Molpipeline, we return a dummy non-empty linear molecule.
+        if scaffold_result.GetNumAtoms() == 0:
+            return (
+                _MurckoScaffoldClusteringHelperPipelineElement.LINEAR_MOLECULE_DUMMY_SCAFFOLD_MOL
+            )
+        return scaffold_result
+
+
+class MurckoScaffoldClustering(ClusterMixin, BaseEstimator):
+    """Murcko scaffold clustering estimator."""
+
+    _parameter_constraints: dict = {
+        "n_jobs": [Integral, None],
+    }
+
+    def __init__(
+        self,
+        *,
+        n_jobs=None,
+        linear_molecules_strategy: str = "ignore",
+    ) -> None:
+        """Initialize Murcko scaffold clustering estimator.
+
+        Parameters
+        ----------
+        n_jobs : int | None
+            Number of jobs to use for parallelization.
+        linear_molecules_strategy : str
+            Strategy for handling linear molecules. Can be "ignore" or "own_cluster". "ignore" will
+            ignore linear molecules and they will be replaced with NaN in the resulting clustering.
+            "own_cluster" will instead cluster linear molecules in their own cluster and give them
+            a valid cluster label.
+        """
+        self.linear_molecules_strategy: str = linear_molecules_strategy
+        self.n_jobs: int | None = n_jobs
+        self.n_clusters_: int | None = None
+        self.labels_: npt.NDArray[np.int32] | None = None
+
+    # pylint: disable=C0103,W0613
+    @_fit_context(prefer_skip_nested_validation=True)
+    def fit(
+        self,
+        X: npt.NDArray[str] | list[str] | npt.NDArray[OptionalMol] | list[OptionalMol],
+        y: npt.NDArray[np.float64] | None = None,
+    ) -> Self:
+        """Fit Murcko scaffold clustering estimator.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples,)
+            Smiles or molecule list or array.
+        y : Ignored
+            Not used, present for API consistency by convention.
+
+        Returns
+        -------
+        Self
+            Fitted estimator.
+        """
+        X = self._validate_data(X, ensure_min_samples=2, ensure_2d=False, dtype=None)
+        return self._fit(X)
+
+    # pylint: disable=C0103,W0613
+    def _fit(
+        self,
+        X: npt.NDArray[str] | list[str] | npt.NDArray[OptionalMol] | list[OptionalMol],
+    ) -> Self:
+        """Fit Murcko scaffold clustering estimator.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples,)
+            Smiles or molecule list or array.
+
+        Returns
+        -------
+        Self
+            Fitted estimator.
+        """
+        if self.linear_molecules_strategy == "ignore":
+            murcko_scaffold = MurckoScaffoldPipelineElement()
+        elif self.linear_molecules_strategy == "own_cluster":
+            murcko_scaffold = _MurckoScaffoldClusteringHelperPipelineElement()
+        else:
+            raise ValueError("Invalid linear_molecules_strategy")
+
+        none_filter = ErrorFilter(
+            {murcko_scaffold.uuid},
+            filter_everything=True,
+            name="ErrorFilter",
+            n_jobs=1,
+            uuid=None,
+        )
+        none_filler = ErrorReplacer.from_error_filter(
+            none_filter,
+            None,
+        )
+
+        cluster_pipeline = Pipeline(
+            [
+                ("smi2mol", SmilesToMolPipelineElement()),
+                ("murcko_scaffold", murcko_scaffold),
+                ("mol2smi", MolToSmilesPipelineElement()),
+                ("none_filter", none_filter),
+                ("reshape2d", FunctionTransformer(func=np.atleast_2d)),
+                ("transpose", FunctionTransformer(func=np.transpose)),
+                ("scaffold_encoder", OrdinalEncoder()),
+                ("reshape1d", FunctionTransformer(func=np.ravel)),
+                ("none_filler", none_filler),
+            ],
+            n_jobs=self.n_jobs,
+        )
+
+        self.labels_ = cluster_pipeline.fit_transform(X, None)
+        self.n_clusters_ = len(np.unique(self.labels_[~np.isnan(self.labels_)]))
+        return self
+
+    # pylint: disable=C0103,W0613
+    def fit_predict(
+        self,
+        X: npt.NDArray[str] | list[str] | npt.NDArray[OptionalMol] | list[OptionalMol],
+        y: npt.NDArray[np.float64] | None = None,
+    ) -> npt.NDArray[np.int32]:
+        """Fit and predict Murcko scaffold clustering estimator.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples,)
+            Smiles or molecule list or array.
+        y : Ignored
+            Not used, present for API consistency by convention.
+
+        Returns
+        -------
+        np.ndarray[int]
+            Cluster labels.
+        """
+        # pylint: disable=W0246
+        return super().fit_predict(X, y)
---
+++ tests/test_sklearn_estimators/test_murcko_scaffold_clustering.py
@@ -0,0 +1,77 @@
+"""Test Murcko scaffold clustering estimator."""
+from __future__ import annotations
+
+import unittest
+
+import numpy as np
+from sklearn.metrics import adjusted_rand_score
+
+from molpipeline.sklearn_estimators.murcko_scaffold_clustering import (
+    MurckoScaffoldClustering,
+)
+
+
+class TestMurckoScaffoldClusteringEstimator(unittest.TestCase):
+    """Test Murcko scaffold clustering estimator."""
+
+    def test_murcko_scaffold_clustering_estimator(self) -> None:
+        """Test Murcko scaffold clustering estimator."""
+
+        scaffold_smiles: list[str] = [
+            "Cc1ccccc1",
+            "Cc1cc(Oc2nccc(CCC)c2)ccc1",
+            "c1ccccc1",
+        ]
+        linear_smiles: list[str] = ["CC", "CCC", "CCCN"]
+
+        estimator: MurckoScaffoldClustering = MurckoScaffoldClustering(
+            n_jobs=1, linear_molecules_strategy="ignore"
+        )
+
+        # test basic scaffold-based clustering works as intended
+        input_smiles = scaffold_smiles
+        self.assertTrue(
+            np.allclose(
+                adjusted_rand_score(estimator.fit_predict(input_smiles), [0, 1, 0]),
+                1.0,
+                atol=1e-10,
+            )
+        )
+        self.assertEqual(estimator.n_clusters_, 2)
+
+        # test linear molecule handling. We expect the linear molecules to be clustered in the same cluster
+        input_smiles = scaffold_smiles + linear_smiles
+        cluster_labels = estimator.fit_predict(input_smiles)
+        self.assertTrue(
+            np.equal(
+                np.isnan(cluster_labels), [False, False, False, True, True, True]
+            ).all()
+        )
+        self.assertTrue(
+            np.allclose(
+                adjusted_rand_score(
+                    cluster_labels[~np.isnan(cluster_labels)], [0, 1, 0]
+                ),
+                1.0,
+                atol=1e-10,
+            )
+        )
+        self.assertEqual(estimator.n_clusters_, 2)
+
+        # create new estimator with "own_cluster" strategy
+        estimator: MurckoScaffoldClustering = MurckoScaffoldClustering(
+            n_jobs=1, linear_molecules_strategy="own_cluster"
+        )
+
+        # test linear molecule handling. We expect the linear molecules to be clustered in the same cluster
+        input_smiles = scaffold_smiles + linear_smiles
+        self.assertTrue(
+            np.allclose(
+                adjusted_rand_score(
+                    estimator.fit_predict(input_smiles), [0, 1, 0, 2, 2, 2]
+                ),
+                1.0,
+                atol=1e-10,
+            )
+        )
+        self.assertEqual(estimator.n_clusters_, 3)
