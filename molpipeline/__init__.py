b5b5da3c1e0ac55dd499e1f9d613e882f3413d7f
Jochen Sieg <jochen.sieg@basf.com>
2024-03-26 17:24:44 +0100

    __init__: fix module import order

--- molpipeline/__init__.py
+++ molpipeline/__init__.py
@@ -1,5 +1,8 @@
 """Initialize the molpipeline package."""
 
+from .error_handling import ErrorFilter, FilterReinserter
+from .pipeline import Pipeline
+
 # pylint: disable=no-name-in-module
 from rdkit.Chem import PropertyPickleOptions, SetDefaultPickleProperties
 
@@ -8,9 +11,6 @@ from .post_prediction import PostPredictionWrapper
 # Keep all properties when pickling. Otherwise, we will lose properties set on RDKitMol when passed to
 # multiprocessing subprocesses.
 SetDefaultPickleProperties(PropertyPickleOptions.AllProps)
-
-from .error_handling import ErrorFilter, FilterReinserter
-from .pipeline import Pipeline
 
 __all__ = [
     "Pipeline",
