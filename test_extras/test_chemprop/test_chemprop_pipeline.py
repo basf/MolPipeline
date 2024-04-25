fc9074c44e49adf25599644ddcb8882ca2bd2e33
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2024-04-25 16:56:42 +0200

    implement tests for regession and classification

--- test_extras/test_chemprop/test_chemprop_pipeline.py
+++ test_extras/test_chemprop/test_chemprop_pipeline.py
@@ -1,9 +1,14 @@
 """Test module for behavior of chemprop in a pipeline."""
 
 import unittest
+from io import BytesIO
+from typing import TypeVar
 
+import joblib
 import numpy as np
+import pandas as pd
 from chemprop.nn.loss import LossFunction
+from lightning import pytorch as pl
 from sklearn.base import clone
 from torch import nn
 
@@ -15,7 +20,11 @@ from molpipeline.estimators.chemprop.component_wrapper import (
     BondMessagePassing,
     SumAggregation,
 )
-from molpipeline.estimators.chemprop.models import ChempropModel
+from molpipeline.estimators.chemprop.models import (
+    ChempropClassifier,
+    ChempropModel,
+    ChempropRegressor,
+)
 from molpipeline.mol2any.mol2chemprop import MolToChemprop
 from molpipeline.pipeline import Pipeline
 from molpipeline.post_prediction import PostPredictionWrapper
@@ -61,6 +70,100 @@ def get_model_pipeline() -> Pipeline:
         ],
     )
     return model_pipeline
+
+
+DEFAULT_TRAINER = pl.Trainer(
+    accelerator="cpu",
+    logger=False,
+    enable_checkpointing=False,
+    max_epochs=5,
+    enable_model_summary=False,
+    enable_progress_bar=False,
+    val_check_interval=0.0,
+)
+
+
+def get_regression_pipeline() -> Pipeline:
+    """Get the Chemprop model pipeline for regression.
+
+    Returns
+    -------
+    Pipeline
+        The Chemprop model pipeline for regression.
+    """
+
+    smiles2mol = SmilesToMol()
+    mol2chemprop = MolToChemprop()
+    error_filter = ErrorFilter(filter_everything=True)
+    filter_reinserter = FilterReinserter.from_error_filter(
+        error_filter, fill_value=np.nan
+    )
+    chemprop_model = ChempropRegressor(lightning_trainer=DEFAULT_TRAINER)
+    model_pipeline = Pipeline(
+        steps=[
+            ("smiles2mol", smiles2mol),
+            ("mol2chemprop", mol2chemprop),
+            ("error_filter", error_filter),
+            ("model", chemprop_model),
+            ("filter_reinserter", PostPredictionWrapper(filter_reinserter)),
+        ],
+    )
+    return model_pipeline
+
+
+def get_classification_pipeline() -> Pipeline:
+    """Get the Chemprop model pipeline for classification.
+
+    Returns
+    -------
+    Pipeline
+        The Chemprop model pipeline for classification.
+    """
+    smiles2mol = SmilesToMol()
+    mol2chemprop = MolToChemprop()
+    error_filter = ErrorFilter(filter_everything=True)
+    filter_reinserter = FilterReinserter.from_error_filter(
+        error_filter, fill_value=np.nan
+    )
+    chemprop_model = ChempropClassifier(lightning_trainer=DEFAULT_TRAINER)
+    model_pipeline = Pipeline(
+        steps=[
+            ("smiles2mol", smiles2mol),
+            ("mol2chemprop", mol2chemprop),
+            ("error_filter", error_filter),
+            ("model", chemprop_model),
+            ("filter_reinserter", PostPredictionWrapper(filter_reinserter)),
+        ],
+    )
+    return model_pipeline
+
+
+_T = TypeVar("_T")
+
+
+def joblib_dump_load(obj: _T) -> _T:
+    """Dump and load an object using joblib.
+
+    Notes
+    -----
+    The object is not dumped to disk but to a BytesIO object.
+
+    Parameters
+    ----------
+    obj : _T
+        The object to dump and load.
+
+    Returns
+    -------
+    _T
+        The loaded object.
+    """
+    bytes_container = BytesIO()
+    joblib.dump(obj, bytes_container)
+    bytes_container.seek(0)  # update to enable reading
+    bytes_model = bytes_container.read()
+
+    return joblib.load(BytesIO(bytes_model))
 
 
 class TestChempropPipeline(unittest.TestCase):
@@ -155,3 +258,60 @@ class TestChempropPipeline(unittest.TestCase):
         proba = pipeline.predict_proba(smiles)
         self.assertEqual(len(proba), 4)
         self.assertTrue(np.isnan(proba[-1]).all())
+
+
+class TestRegressionPipeline(unittest.TestCase):
+    """Test the Chemprop model pipeline for regression."""
+
+    def test_prediction(self) -> None:
+        """Test the prediction of the regression model."""
+
+        molecule_net_logd_df = pd.read_csv(
+            "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/Lipophilicity.csv"
+        ).head(1000)
+        regression_model = get_regression_pipeline()
+        regression_model.fit(
+            molecule_net_logd_df["smiles"].tolist(),
+            molecule_net_logd_df["exp"].to_numpy(),
+        )
+        pred = regression_model.predict(molecule_net_logd_df["smiles"].tolist())
+
+        self.assertEqual(len(pred), len(molecule_net_logd_df))
+
+        model_copy = joblib_dump_load(regression_model)
+        pred_copy = model_copy.predict(molecule_net_logd_df["smiles"].tolist())
+        self.assertTrue(np.allclose(pred, pred_copy))
+
+
+class TestClassificationPipeline(unittest.TestCase):
+    """Test the Chemprop model pipeline for classification."""
+
+    def test_prediction(self) -> None:
+        """Test the prediction of the classification model."""
+
+        molecule_net_bbbp_df = pd.read_csv(
+            "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/BBBP.csv"
+        ).head(1000)
+        classification_model = get_classification_pipeline()
+        classification_model.fit(
+            molecule_net_bbbp_df["smiles"].tolist(),
+            molecule_net_bbbp_df["p_np"].to_numpy(),
+        )
+        pred = classification_model.predict(molecule_net_bbbp_df["smiles"].tolist())
+        proba = classification_model.predict_proba(
+            molecule_net_bbbp_df["smiles"].tolist()
+        )
+        self.assertEqual(len(pred), len(molecule_net_bbbp_df))
+        self.assertEqual(proba.shape[1], 2)
+        self.assertEqual(proba.shape[0], len(molecule_net_bbbp_df))
+
+        model_copy = joblib_dump_load(classification_model)
+        pred_copy = model_copy.predict(molecule_net_bbbp_df["smiles"].tolist())
+        proba_copy = model_copy.predict_proba(molecule_net_bbbp_df["smiles"].tolist())
+
+        nan_indices = np.isnan(pred)
+        self.assertListEqual(nan_indices.tolist(), np.isnan(pred_copy).tolist())
+        self.assertTrue(np.allclose(pred[~nan_indices], pred_copy[~nan_indices]))
+
+        self.assertEqual(proba.shape, proba_copy.shape)
+        self.assertTrue(np.allclose(proba[~nan_indices], proba_copy[~nan_indices]))
