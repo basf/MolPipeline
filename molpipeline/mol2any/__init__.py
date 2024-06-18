42f5f792a67be636430ab201829e0df6fb785554
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2024-06-18 12:21:52 +0200

    add maccs key to init

--- molpipeline/mol2any/__init__.py
+++ molpipeline/mol2any/__init__.py
@@ -3,6 +3,7 @@
 from molpipeline.mol2any.mol2bin import MolToBinary
 from molpipeline.mol2any.mol2concatinated_vector import MolToConcatenatedVector
 from molpipeline.mol2any.mol2inchi import MolToInchi, MolToInchiKey
+from molpipeline.mol2any.mol2maccs_key_fingerprint import MolToMACCSFP
 from molpipeline.mol2any.mol2morgan_fingerprint import MolToMorganFP
 from molpipeline.mol2any.mol2net_charge import MolToNetCharge
 from molpipeline.mol2any.mol2path_fingerprint import Mol2PathFP
@@ -13,6 +14,7 @@ __all__ = [
     "MolToBinary",
     "MolToConcatenatedVector",
     "MolToSmiles",
+    "MolToMACCSFP",
     "MolToMorganFP",
     "MolToNetCharge",
     "Mol2PathFP",
