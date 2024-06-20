7978aab843d91969cc4d9269d1a072758a51bfa1
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2024-06-20 15:32:07 +0200

    Add comments

--- test_extras/test_chemprop/chemprop_test_utils/constant_vars.py
+++ test_extras/test_chemprop/chemprop_test_utils/constant_vars.py
@@ -1,5 +1,7 @@
 """Variables that are used in multiple tests."""
 
+# These are model parameters which are copied by value, but are too complex to check for equality.
+# Thus, for these model parameters, only the type is checked.
 NO_IDENTITY_CHECK = [
     "model__agg",
     "model__message_passing",
