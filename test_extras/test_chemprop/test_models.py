c5810fc5fe8432f93878d4595107ce9d99e64a21
hemmerj3 <jennifer.hemmerich@basf.com>
2024-09-03 16:23:01 +0200

    test multiclass setter and getter

--- test_extras/test_chemprop/test_models.py
+++ test_extras/test_chemprop/test_models.py
@@ -29,9 +29,10 @@ from molpipeline.utils.json_operations import recursive_from_json, recursive_to_
 # pylint: disable=relative-beyond-top-level
 from test_extras.test_chemprop.chemprop_test_utils.compare_models import compare_params
 from test_extras.test_chemprop.chemprop_test_utils.constant_vars import (
-    DEFAULT_PARAMS,
     NO_IDENTITY_CHECK,
     DEFAULT_SET_PARAMS,
+    DEFAULT_BINARY_CLASSIFICATION_PARAMS,
+    DEFAULT_MULTICLASS_CLASSIFICATION_PARAMS
 )
 from test_extras.test_chemprop.chemprop_test_utils.default_models import (
     get_chemprop_model_binary_classification_mpnn,
@@ -47,7 +48,7 @@ class TestChempropModel(unittest.TestCase):
         """Test the get_params and set_params methods."""
         chemprop_model = get_chemprop_model_binary_classification_mpnn()
         orig_params = chemprop_model.get_params(deep=True)
-        expected_params = dict(DEFAULT_PARAMS)  # Shallow copy
+        expected_params = dict(DEFAULT_BINARY_CLASSIFICATION_PARAMS)  # Shallow copy
 
         self.assertSetEqual(set(orig_params), set(expected_params))
         # Check if the parameters are as expected
@@ -114,8 +115,8 @@ class TestChempropModel(unittest.TestCase):
         chemprop_model_copy = recursive_from_json(chemprop_json)
         param_dict = chemprop_model_copy.get_params(deep=True)
 
-        self.assertSetEqual(set(param_dict.keys()), set(DEFAULT_PARAMS.keys()))
-        for param_name, param in DEFAULT_PARAMS.items():
+        self.assertSetEqual(set(param_dict.keys()), set(DEFAULT_BINARY_CLASSIFICATION_PARAMS.keys()))
+        for param_name, param in DEFAULT_BINARY_CLASSIFICATION_PARAMS.items():
             if param_name in NO_IDENTITY_CHECK:
                 if isinstance(param, Iterable):
                     self.assertIsInstance(param_dict[param_name], type(param))
@@ -141,7 +142,7 @@ class TestChempropClassifier(unittest.TestCase):
         """Test the get_params and set_params methods."""
         chemprop_model = ChempropClassifier(lightning_trainer__accelerator="cpu")
         param_dict = chemprop_model.get_params(deep=True)
-        expected_params = dict(DEFAULT_PARAMS)  # Shallow copy
+        expected_params = dict(DEFAULT_BINARY_CLASSIFICATION_PARAMS)  # Shallow copy
         self.assertSetEqual(set(param_dict.keys()), set(expected_params.keys()))
         for param_name, param in expected_params.items():
             if param_name in NO_IDENTITY_CHECK:
@@ -173,7 +174,7 @@ class TestChempropRegressor(unittest.TestCase):
         """Test the get_params and set_params methods."""
         chemprop_model = ChempropRegressor(lightning_trainer__accelerator="cpu")
         param_dict = chemprop_model.get_params(deep=True)
-        expected_params = dict(DEFAULT_PARAMS)
+        expected_params = dict(DEFAULT_BINARY_CLASSIFICATION_PARAMS)
         expected_params["model__predictor"] = RegressionFFN
         expected_params["model__predictor__criterion"] = MSELoss
         self.assertSetEqual(set(param_dict.keys()), set(expected_params.keys()))
@@ -200,7 +201,8 @@ class TestChempropMulticlassClassifier(unittest.TestCase):
         n_classes = 3
         chemprop_model = ChempropMulticlassClassifier(lightning_trainer__accelerator="cpu", n_classes=n_classes)
         param_dict = chemprop_model.get_params(deep=True)
-        expected_params = dict(DEFAULT_PARAMS)  # Shallow copy
+        expected_params = dict(DEFAULT_MULTICLASS_CLASSIFICATION_PARAMS)  # Shallow copy
+        expected_params["model__predictor__n_classes"] = n_classes
         expected_params["n_classes"] = n_classes
         self.assertSetEqual(set(param_dict.keys()), set(expected_params.keys()))
         for param_name, param in expected_params.items():
@@ -213,6 +215,8 @@ class TestChempropMulticlassClassifier(unittest.TestCase):
                     self.assertIsInstance(param_dict[param_name], param)
                 else:
                     raise ValueError(f"{param_name} should be a type.")
+            elif (isinstance(param, torch.Tensor)):
+                self.assertTrue(torch.allclose(param_dict[param_name], param))
             else:
                 self.assertEqual(
                     param_dict[param_name], param, f"Test failed for {param_name}"
@@ -230,15 +234,18 @@ class TestChempropMulticlassClassifier(unittest.TestCase):
     
     def test_error_for_multiclass_predictor(self) -> None:
         """Test the error for using a multiclass predictor for a binary classification model."""
-        chemprop_model = ChempropMulticlassClassifier(lightning_trainer__accelerator="cpu", n_classes=3)
-        with self.assertRaises(ValueError):
-            chemprop_model.set_params(model__predictor=RegressionFFN)
         bond_encoder = BondMessagePassing()
         agg = SumAggregation()
-        predictor = MulticlassClassificationFFN(n_classes=2)
         with self.assertRaises(ValueError):
+            predictor = MulticlassClassificationFFN(n_classes=2)
             model = MPNN(message_passing=bond_encoder, agg=agg, predictor=predictor)
-        predictor = RegressionFFN()
+            chemprop_model = ChempropMulticlassClassifier(n_classes=2, model=model)
         with self.assertRaises(ValueError):
+            predictor = MulticlassClassificationFFN(n_classes=3)
             model = MPNN(message_passing=bond_encoder, agg=agg, predictor=predictor)
+            chemprop_model = ChempropMulticlassClassifier(n_classes=4, model=model)
+        with self.assertRaises(AttributeError):
+            predictor = RegressionFFN()
+            model = MPNN(message_passing=bond_encoder, agg=agg, predictor=predictor)
+            chemprop_model = ChempropMulticlassClassifier(n_classes=4, model=model)
 
