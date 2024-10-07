a6c175d8f89326a786e79e3d8a080778744614d3
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2024-10-07 17:58:09 +0200

    move definition of pipeline to utils function

--- tests/test_utils/test_comparison.py
+++ tests/test_utils/test_comparison.py
@@ -2,55 +2,8 @@
 
 from unittest import TestCase
 
-from sklearn.ensemble import RandomForestClassifier
-
-from molpipeline import Pipeline
-from molpipeline.any2mol import SmilesToMol
-from molpipeline.error_handling import ErrorFilter, FilterReinserter
-from molpipeline.mol2any import (
-    MolToConcatenatedVector,
-    MolToMorganFP,
-    MolToRDKitPhysChem,
-)
-from molpipeline.post_prediction import PostPredictionWrapper
 from molpipeline.utils.comparison import check_pipelines_equivalent
-
-
-def get_test_pipeline() -> Pipeline:
-    """Get a test pipeline.
-
-    Returns
-    -------
-    Pipeline
-        Test pipeline.
-    """
-    error_filter = ErrorFilter(filter_everything=True)
-    pipeline = Pipeline(
-        [
-            ("smi2mol", SmilesToMol()),
-            (
-                "mol2fp",
-                MolToConcatenatedVector(
-                    [
-                        ("morgan", MolToMorganFP(n_bits=2048)),
-                        ("physchem", MolToRDKitPhysChem()),
-                    ]
-                ),
-            ),
-            ("error_filter", error_filter),
-            ("rf", RandomForestClassifier()),
-            (
-                "filter_reinserter",
-                PostPredictionWrapper(
-                    FilterReinserter.from_error_filter(error_filter, None)
-                ),
-            ),
-        ],
-        n_jobs=1,
-    )
-
-    # Set up pipeline
-    return pipeline
+from tests.utils.default_models import get_morgan_physchem_rf_pipeline
 
 
 class TestComparison(TestCase):
@@ -59,20 +12,20 @@ class TestComparison(TestCase):
     def test_are_equal(self) -> None:
         """Test if two equivalent pipelines are detected as such."""
 
-        pipeline_a = get_test_pipeline()
-        pipeline_b = get_test_pipeline()
+        pipeline_a = get_morgan_physchem_rf_pipeline()
+        pipeline_b = get_morgan_physchem_rf_pipeline()
         self.assertTrue(check_pipelines_equivalent(pipeline_a, pipeline_b))
 
     def test_are_not_equal(self) -> None:
         """Test if two different pipelines are detected as such."""
         # Test changed parameters
-        pipeline_a = get_test_pipeline()
-        pipeline_b = get_test_pipeline()
+        pipeline_a = get_morgan_physchem_rf_pipeline()
+        pipeline_b = get_morgan_physchem_rf_pipeline()
         pipeline_b.set_params(mol2fp__morgan__n_bits=1024)
         self.assertFalse(check_pipelines_equivalent(pipeline_a, pipeline_b))
 
         # Test changed steps
-        pipeline_b = get_test_pipeline()
+        pipeline_b = get_morgan_physchem_rf_pipeline()
         last_step = pipeline_b.steps[-1]
         pipeline_b.steps = pipeline_b.steps[:-1]
         self.assertFalse(check_pipelines_equivalent(pipeline_a, pipeline_b))
