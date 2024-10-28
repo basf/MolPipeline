8c46fde041a5d9b52500b2ae09a95b4c6a80a5d5
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2024-10-28 16:56:05 +0100

    import missing types

--- tests/test_utils/test_comparison.py
+++ tests/test_utils/test_comparison.py
@@ -3,6 +3,7 @@
 from typing import Callable
 from unittest import TestCase
 
+from molpipeline import Pipeline
 from molpipeline.utils.comparison import check_pipelines_equivalent
 from tests.utils.default_models import (
     get_morgan_physchem_rf_pipeline,
@@ -16,11 +17,11 @@ class TestComparison(TestCase):
     def test_are_equal(self) -> None:
         """Test if two equivalent pipelines are detected as such."""
         # Test standardization pipelines
-        pipline_method_list = [
+        pipline_method_list: list[Callable[[int], Pipeline]] = [
             get_standardization_pipeline,
             get_morgan_physchem_rf_pipeline,
         ]
-        for pipeline_method in pipline_method_list:  # type: Callable[[int], Pipeline]
+        for pipeline_method in pipline_method_list:
             pipeline_a = pipeline_method()
             pipeline_b = pipeline_method()
             self.assertTrue(check_pipelines_equivalent(pipeline_a, pipeline_b))
