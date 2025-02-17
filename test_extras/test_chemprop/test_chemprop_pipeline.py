38bd8c8bfa685debd3b19d1b93b8beedf872fb91
Christian W. Feldmann <128160984+c-w-feldmann@users.noreply.github.com>
2025-02-17 15:57:42 +0100

    Ensure compatibility with sklearn and implement tags (#124)
    * add unit test to reproduce error
    
    * fix some metadata routing
    
    * isort + black
    
    * move compatibility tests to separate test class
    
    * ignore too many lines and remove ignores locally flagged `useless-suppression` annotations
    
    * remove ignores flagged `useless-suppression` annotations
    
    * add test for ChEMPROP Model, which fails
    
    * set tags for chemprop models
    
    * remove invalid smiles

--- test_extras/test_chemprop/test_chemprop_pipeline.py
+++ test_extras/test_chemprop/test_chemprop_pipeline.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 from lightning import pytorch as pl
 from sklearn.base import clone
+from sklearn.calibration import CalibratedClassifierCV
 
 from molpipeline.any2mol import SmilesToMol
 from molpipeline.error_handling import ErrorFilter, FilterReinserter
@@ -25,11 +26,39 @@ from molpipeline.estimators.chemprop.models import (
     ChempropMulticlassClassifier,
     ChempropRegressor,
 )
+from molpipeline.mol2any import MolToSmiles
 from molpipeline.mol2any.mol2chemprop import MolToChemprop
 from molpipeline.pipeline import Pipeline
 from molpipeline.post_prediction import PostPredictionWrapper
 from test_extras.test_chemprop.chemprop_test_utils.compare_models import compare_params
 from tests import TEST_DATA_DIR
+
+
+def get_smiles_checker_pipeline() -> Pipeline:
+    """Get a pipeline that reads and writes the SMILES string.
+
+    Invalid SMILES strings are replaced with NaN.
+
+    Returns
+    -------
+    Pipeline
+        The pipeline that reads and writes the SMILES string.
+    """
+    smiles2mol = SmilesToMol()
+    mol2smiles = MolToSmiles()
+    error_filter = ErrorFilter(filter_everything=True)
+    filter_reinserter = FilterReinserter.from_error_filter(
+        error_filter, fill_value=np.nan
+    )
+    smiles_pipeline = Pipeline(
+        [
+            ("smiles2mol", smiles2mol),
+            ("error_filter", error_filter),
+            ("mol2smiles", mol2smiles),
+            ("filter_reinserter", filter_reinserter),
+        ],
+    )
+    return smiles_pipeline
 
 
 # pylint: disable=duplicate-code
@@ -317,28 +346,41 @@ class TestRegressionPipeline(unittest.TestCase):
 class TestClassificationPipeline(unittest.TestCase):
     """Test the Chemprop model pipeline for classification."""
 
-    def test_prediction(self) -> None:
-        """Test the prediction of the classification model."""
-
+    def setUp(self) -> None:
+        """Set up repeated variables."""
         molecule_net_bbbp_df = pd.read_csv(
             TEST_DATA_DIR / "molecule_net_bbbp.tsv.gz", sep="\t", nrows=100
         )
+        smiles_pipeline = get_smiles_checker_pipeline()
+        molecule_net_bbbp_df["smiles"] = smiles_pipeline.transform(
+            molecule_net_bbbp_df["smiles"]
+        )
+        molecule_net_bbbp_df = molecule_net_bbbp_df.dropna(subset=["smiles", "p_np"])
+
+        self.molecule_net_bbbp_df = molecule_net_bbbp_df
+
+    def test_prediction(self) -> None:
+        """Test the prediction of the classification model."""
         classification_model = get_classification_pipeline()
         classification_model.fit(
-            molecule_net_bbbp_df["smiles"].tolist(),
-            molecule_net_bbbp_df["p_np"].to_numpy(),
+            self.molecule_net_bbbp_df["smiles"].tolist(),
+            self.molecule_net_bbbp_df["p_np"].to_numpy(),
         )
-        pred = classification_model.predict(molecule_net_bbbp_df["smiles"].tolist())
+        pred = classification_model.predict(
+            self.molecule_net_bbbp_df["smiles"].tolist()
+        )
         proba = classification_model.predict_proba(
-            molecule_net_bbbp_df["smiles"].tolist()
+            self.molecule_net_bbbp_df["smiles"].tolist()
         )
-        self.assertEqual(len(pred), len(molecule_net_bbbp_df))
+        self.assertEqual(len(pred), len(self.molecule_net_bbbp_df))
         self.assertEqual(proba.shape[1], 2)
-        self.assertEqual(proba.shape[0], len(molecule_net_bbbp_df))
+        self.assertEqual(proba.shape[0], len(self.molecule_net_bbbp_df))
 
         model_copy = joblib_dump_load(classification_model)
-        pred_copy = model_copy.predict(molecule_net_bbbp_df["smiles"].tolist())
-        proba_copy = model_copy.predict_proba(molecule_net_bbbp_df["smiles"].tolist())
+        pred_copy = model_copy.predict(self.molecule_net_bbbp_df["smiles"].tolist())
+        proba_copy = model_copy.predict_proba(
+            self.molecule_net_bbbp_df["smiles"].tolist()
+        )
 
         nan_indices = np.isnan(pred)
         self.assertListEqual(nan_indices.tolist(), np.isnan(pred_copy).tolist())
@@ -349,13 +391,39 @@ class TestClassificationPipeline(unittest.TestCase):
 
         # Test single prediction, this was causing an error before
         single_mol_pred = classification_model.predict(
-            [molecule_net_bbbp_df["smiles"].iloc[0]]
+            [self.molecule_net_bbbp_df["smiles"].iloc[0]]
         )
         self.assertEqual(single_mol_pred.shape, (1,))
         single_mol_proba = classification_model.predict_proba(
-            [molecule_net_bbbp_df["smiles"].iloc[0]]
+            [self.molecule_net_bbbp_df["smiles"].iloc[0]]
         )
         self.assertEqual(single_mol_proba.shape, (1, 2))
+
+    def test_calibrated_classifier(self) -> None:
+        """Test if the pipeline can be used with a CalibratedClassifierCV."""
+        calibrated_pipeline = CalibratedClassifierCV(
+            get_classification_pipeline(), cv=2, ensemble=True, method="isotonic"
+        )
+        calibrated_pipeline.fit(
+            self.molecule_net_bbbp_df["smiles"].tolist(),
+            self.molecule_net_bbbp_df["p_np"].to_numpy(),
+        )
+        predicted_value_array = calibrated_pipeline.predict(
+            self.molecule_net_bbbp_df["smiles"].tolist()
+        )
+        predicted_proba_array = calibrated_pipeline.predict_proba(
+            self.molecule_net_bbbp_df["smiles"].tolist()
+        )
+        self.assertIsInstance(predicted_value_array, np.ndarray)
+        self.assertIsInstance(predicted_proba_array, np.ndarray)
+        self.assertEqual(
+            predicted_value_array.shape,
+            (len(self.molecule_net_bbbp_df["smiles"].tolist()),),
+        )
+        self.assertEqual(
+            predicted_proba_array.shape,
+            (len(self.molecule_net_bbbp_df["smiles"].tolist()), 2),
+        )
 
 
 class TestMulticlassClassificationPipeline(unittest.TestCase):
