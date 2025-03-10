91fb8b4f97a44d65e1e63176e9c961cfcf42cf55
Jochen Sieg <jochen.sieg@basf.com>
2025-03-10 17:52:51 +0100

    move version from pyproject.toml to __init__

---
+++ tests/test_init.py
@@ -0,0 +1,18 @@
+"""Test functionality set at package init."""
+
+import unittest
+
+from molpipeline import __version__
+
+
+class TestInit(unittest.TestCase):
+
+    def test_version(self):
+        """Test that the package has a version."""
+        self.assertIsInstance(__version__, str)
+        splitted = __version__.split(".")
+        self.assertEqual(len(splitted), 3)
+        major, minor, patch = splitted
+        self.assertTrue(major.isdigit())
+        self.assertTrue(minor.isdigit())
+        self.assertTrue(patch.isdigit())
