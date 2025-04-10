b0d0d8979839019531c42fe06c680eb0a15367cc
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2025-04-10 19:58:59 +0200

    move back to isort

--- molpipeline/mol2mol/standardization.py
+++ molpipeline/mol2mol/standardization.py
@@ -11,8 +11,8 @@ except ImportError:
 
 from rdkit import Chem
 from rdkit.Chem import SaltRemover as rdkit_SaltRemover
+from rdkit.Chem import SanitizeMol  # pylint: disable=no-name-in-module
 from rdkit.Chem import (
-    SanitizeMol,  # pylint: disable=no-name-in-module
     rdMolHash,
     rdmolops,
 )
