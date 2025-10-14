505a74b32c35399c93d8675d0128415c9887d319
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2025-10-14 11:33:12 +0200

    linting

--- molpipeline/__init__.py
+++ molpipeline/__init__.py
@@ -7,8 +7,8 @@ from molpipeline.error_handling import ErrorFilter, FilterReinserter
 from molpipeline.pipeline import Pipeline
 from molpipeline.post_prediction import PostPredictionWrapper
 
-# Keep all properties when pickling. Otherwise, we will lose properties set on RDKitMol when passed to
-# multiprocessing subprocesses.
+# Keep all properties when pickling. Otherwise, we will lose properties set on RDKitMol
+# when passed to multiprocessing subprocesses.
 SetDefaultPickleProperties(PropertyPickleOptions.AllProps)
 
 __all__ = [
