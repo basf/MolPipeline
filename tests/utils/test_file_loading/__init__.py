e34e9819a32cd643be80a52e65695628e6a61ec8
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2025-12-02 17:11:43 +0100

    Implement unit tests

---
+++ tests/utils/test_file_loading/__init__.py
@@ -0,0 +1,1 @@
+"""Unit tests for file loading utilities."""
---
+++ tests/utils/test_file_loading/test_url_loading.py
@@ -0,0 +1,36 @@
+"""Tests for URL file loading utilities."""
+
+import unittest
+
+from molpipeline.utils.file_loading.url_file_loading import URLFileLoader
+
+
+class TestURLFileLoader(unittest.TestCase):
+    """Unit tests for the URLFileLoader class."""
+
+    def setUp(self) -> None:
+        """Set up test variables."""
+        self.test_url = "https://zenodo.org/records/15460715/files/chemeleon_mp.pt"
+        self.loader = URLFileLoader(url=self.test_url, timeout=10)
+
+    def test_initialization(self) -> None:
+        """Test initialization of URLFileLoader."""
+        self.assertEqual(self.loader.url, self.test_url)
+        self.assertEqual(self.loader.timeout, 10)
+
+    def test_get_params(self) -> None:
+        """Test get_params method."""
+        params = self.loader.get_params()
+        self.assertEqual(params, {"url": self.test_url, "timeout": 10})
+
+    def test_set_params(self) -> None:
+        """Test set_params method."""
+        new_url = "https://www.test.com"
+        self.loader.set_params(url=new_url)
+        self.assertEqual(self.loader.url, new_url)
+
+    def test_load_file(self) -> None:
+        """Test load_file method."""
+        content = self.loader.load_file()
+        self.assertIsInstance(content, bytes)
+        self.assertGreater(len(content), 0)
