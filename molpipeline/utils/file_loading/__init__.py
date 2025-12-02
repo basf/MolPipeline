91b41ef72638253e114df157fb42f5c80165cf07
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2025-12-02 22:17:05 +0100

    imports URLFileLoader dynamically

--- molpipeline/utils/file_loading/__init__.py
+++ molpipeline/utils/file_loading/__init__.py
@@ -2,13 +2,15 @@
 
 import importlib.util
 
-from molpipeline.utils.file_loading.url_file_loading import URLFileLoader
+__all__ = []
 
-__all__ = ["URLFileLoader"]
+if importlib.util.find_spec("requests") is not None:
+    from molpipeline.utils.file_loading.url_file_loading import URLFileLoader
+
+    __all__ += ["URLFileLoader"]
 
 
-dvc_spec = importlib.util.find_spec("dvc")
-if dvc_spec is not None:
+if importlib.util.find_spec("dvc") is not None:
     from molpipeline.utils.file_loading.dvc_file_loading import DVCFileLoader
 
     __all__ += ["DVCFileLoader"]
