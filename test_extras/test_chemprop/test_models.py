ad8523b3dd81619f02912d0c2378b8e6bd18bbf1
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2025-12-04 19:59:14 +0100

    Move randomization to func

--- test_extras/test_chemprop/test_models.py
+++ test_extras/test_chemprop/test_models.py
@@ -42,6 +42,45 @@ from test_extras.test_chemprop.chemprop_test_utils.default_models import (
 logging.getLogger("lightning.pytorch.utilities.rank_zero").setLevel(logging.WARNING)  # pylint: disable=no-member
 
 
+def randomize_state_dict_weights(
+    state_dict: dict[str, torch.Tensor],
+    random_state: int | None = None,
+) -> dict[str, torch.Tensor]:
+    """Randomize the weights in a state_dict.
+
+    Parameters
+    ----------
+    state_dict : dict[str, torch.Tensor]
+        The state_dict to randomize.
+    random_state : int | None, default=None
+        The random state to use for reproducibility.
+
+    Returns
+    -------
+    dict[str, torch.Tensor]
+        The randomized state_dict.
+
+    Raises
+    ------
+    ValueError
+        If the state dict does not contain any weights to randomize.
+
+    """
+    random_state_dict = {}
+    rng = np.random.default_rng(random_state)
+    altered_keys = []
+    for key, value in state_dict.items():
+        if key.endswith(".weight"):
+            altered_keys.append(key)
+            random_weights = rng.normal(size=value.shape).astype(np.float32)
+            random_state_dict[key] = torch.tensor(random_weights)
+        else:
+            random_state_dict[key] = value
+    if not altered_keys:
+        raise ValueError("No weights were altered in the state_dict.")
+    return random_state_dict
+
+
 class TestChempropModel(unittest.TestCase):
     """Test the Chemprop model."""
 
@@ -162,26 +201,14 @@ class TestChempropModel(unittest.TestCase):
         ----
         Testing that the predictions agree is done in test_chemprop_pipeline.py.
 
-        Raises:
-        ------
-        AssertionError
-            If no weights were altered in the state_dict.
-
         """
         # Create a Chemprop model that is used to define the state_dict
         chemprop_classifier = get_chemprop_model_binary_classification_mpnn()
 
-        rng = np.random.default_rng(20251204)
-        random_state_dict = {}
-        altered_keys = []
-        for key, value in chemprop_classifier.model.state_dict().items():
-            if key.endswith(".weight"):
-                altered_keys.append(key)
-                random_weights = rng.normal(size=value.shape).astype(np.float32)
-                random_state_dict[key] = torch.tensor(random_weights)
-            random_state_dict[key] = value
-        if not altered_keys:
-            raise AssertionError("No weights were altered in the state_dict.")
+        random_state_dict = randomize_state_dict_weights(
+            chemprop_classifier.model.state_dict(),
+            random_state=20251204,
+        )
 
         # Test passing the state_dict directly
         new_model = ChempropModel(
