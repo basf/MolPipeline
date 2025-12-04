010552ecbd1e9c49a23b50f71759236b1eb39b00
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2025-12-04 20:06:28 +0100

    move function

--- test_extras/test_chemprop/test_models.py
+++ test_extras/test_chemprop/test_models.py
@@ -6,7 +6,6 @@ import unittest
 from collections.abc import Iterable
 from pathlib import Path
 
-import numpy as np
 import torch
 from chemprop.nn.metrics import MSE
 from sklearn.base import clone
@@ -38,47 +37,11 @@ from test_extras.test_chemprop.chemprop_test_utils.constant_vars import (
 from test_extras.test_chemprop.chemprop_test_utils.default_models import (
     get_chemprop_model_binary_classification_mpnn,
 )
+from test_extras.test_chemprop.chemprop_test_utils.randomization import (
+    randomize_state_dict_weights,
+)
 
 logging.getLogger("lightning.pytorch.utilities.rank_zero").setLevel(logging.WARNING)  # pylint: disable=no-member
-
-
-def randomize_state_dict_weights(
-    state_dict: dict[str, torch.Tensor],
-    random_state: int | None = None,
-) -> dict[str, torch.Tensor]:
-    """Randomize the weights in a state_dict.
-
-    Parameters
-    ----------
-    state_dict : dict[str, torch.Tensor]
-        The state_dict to randomize.
-    random_state : int | None, default=None
-        The random state to use for reproducibility.
-
-    Returns
-    -------
-    dict[str, torch.Tensor]
-        The randomized state_dict.
-
-    Raises
-    ------
-    ValueError
-        If the state dict does not contain any weights to randomize.
-
-    """
-    random_state_dict = {}
-    rng = np.random.default_rng(random_state)
-    altered_keys = []
-    for key, value in state_dict.items():
-        if key.endswith(".weight"):
-            altered_keys.append(key)
-            random_weights = rng.normal(size=value.shape).astype(np.float32)
-            random_state_dict[key] = torch.tensor(random_weights)
-        else:
-            random_state_dict[key] = value
-    if not altered_keys:
-        raise ValueError("No weights were altered in the state_dict.")
-    return random_state_dict
 
 
 class TestChempropModel(unittest.TestCase):
