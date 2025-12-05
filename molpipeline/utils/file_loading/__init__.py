15cee8cb1ccbac11b297440bc1fde49bed45f1b2
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2025-12-05 10:17:45 +0100

    Expose ABCFileLoader

--- molpipeline/utils/file_loading/__init__.py
+++ molpipeline/utils/file_loading/__init__.py
@@ -2,7 +2,11 @@
 
 import importlib.util
 
-__all__ = []
+from molpipeline.utils.file_loading.dvc_file_loading import ABCFileLoader
+
+__all__ = [
+    "ABCFileLoader",
+]
 
 if importlib.util.find_spec("requests") is not None:
     from molpipeline.utils.file_loading.url_file_loading import URLFileLoader
