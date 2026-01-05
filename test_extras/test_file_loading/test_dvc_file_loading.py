855690abb92d199f73af2728dad86dcee6e8b6a0
Christian W. Feldmann <128160984+c-w-feldmann@users.noreply.github.com>
2026-01-05 14:12:42 +0100

    Bug: FileLoader.get_params has invalid signature (#225)
    * Reproduce error with tests
    
    * Add param deep

--- test_extras/test_file_loading/test_dvc_file_loading.py
+++ test_extras/test_file_loading/test_dvc_file_loading.py
@@ -5,8 +5,10 @@ import unittest
 from unittest import mock
 
 import pandas as pd
+from sklearn.base import clone
 
 from molpipeline.utils.file_loading.dvc_file_loading import DVCFileLoader
+from molpipeline.utils.json_operations import recursive_from_json, recursive_to_json
 
 
 class TestDVCFileLoading(unittest.TestCase):
@@ -63,3 +65,21 @@ class TestDVCFileLoading(unittest.TestCase):
         self.assertEqual(params["file_path"], new_file_path)
         self.assertEqual(params["repo"], new_repo)
         self.assertEqual(params["rev"], new_rev)
+
+    def test_serialization(self) -> None:
+        """Test serialization and deserialization of the DVC file loader."""
+        json_dict = recursive_to_json(self.dvc_loader)
+        deserialized_loader = recursive_from_json(json_dict)
+        self.assertIsInstance(deserialized_loader, DVCFileLoader)
+        self.assertEqual(deserialized_loader.file_path, self.dvc_loader.file_path)
+        self.assertEqual(deserialized_loader.repo, self.dvc_loader.repo)
+        self.assertEqual(deserialized_loader.rev, self.dvc_loader.rev)
+
+    def test_cloning(self) -> None:
+        """Test cloning of the DVC file loader."""
+        cloned_loader: DVCFileLoader = clone(self.dvc_loader)  # type: ignore
+        self.assertNotEqual(cloned_loader, self.dvc_loader)
+        self.assertIsInstance(cloned_loader, DVCFileLoader)
+        self.assertEqual(cloned_loader.file_path, self.dvc_loader.file_path)
+        self.assertEqual(cloned_loader.repo, self.dvc_loader.repo)
+        self.assertEqual(cloned_loader.rev, self.dvc_loader.rev)
