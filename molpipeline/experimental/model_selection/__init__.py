75c63baf51726bb1d0c39c7d7af3c9cf66a47ef7
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2026-02-10 12:03:45 +0100

    Add AddOneGroupSplit and TimeThresholdSplitter to init

--- molpipeline/experimental/model_selection/__init__.py
+++ molpipeline/experimental/model_selection/__init__.py
@@ -4,5 +4,16 @@ from molpipeline.experimental.model_selection.splitter import (
     GroupShuffleSplit,
     create_continuous_stratified_folds,
 )
+from molpipeline.experimental.model_selection.splitter.add_one_group_split import (
+    AddOneGroupSplit,
+)
+from molpipeline.experimental.model_selection.splitter.time_threshold_splitter import (
+    TimeThresholdSplitter,
+)
 
-__all__ = ["GroupShuffleSplit", "create_continuous_stratified_folds"]
+__all__ = [
+    "AddOneGroupSplit",
+    "GroupShuffleSplit",
+    "TimeThresholdSplitter",
+    "create_continuous_stratified_folds",
+]
