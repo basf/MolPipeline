f84202c9bdc9413d5f9271a4cf0b38bee6f3bc6a
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2026-02-19 17:15:14 +0100

    Ruff format

--- tests/test_elements/test_error_handling.py
+++ tests/test_elements/test_error_handling.py
@@ -245,8 +245,10 @@ class NoneTest(unittest.TestCase):
         )
         pipeline2 = clone(pipeline)
 
-        expected_msg = (r"could not broadcast input array from shape \(0,\) into shape"
-                        r"\(3,\)")
+        expected_msg = (
+            r"could not broadcast input array from shape \(0,\) into shape"
+            r"\(3,\)"
+        )
         with self.assertRaisesRegex(ValueError, expected_msg):
             pipeline.transform(test_values)
         with self.assertRaisesRegex(ValueError, expected_msg):
