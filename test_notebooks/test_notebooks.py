b910abb8acda344c0013f0c7b5da671839049794
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2026-02-23 18:02:04 +0100

    restructure tests

--- test_notebooks/test_notebooks.py
+++ test_notebooks/test_notebooks.py
@@ -1,46 +1,17 @@
 """Script that runs all notebooks once to check if they execute without error."""
 
-import argparse
-import subprocess
-import sys
+import unittest
 from pathlib import Path
 
+import nbformat
 from loguru import logger
+from nbconvert.preprocessors import CellExecutionError, ExecutePreprocessor
 
 # list of directories containing notebooks
-NOTEBOOK_DIRS = ["notebooks"]
+NOTEBOOK_DIR = Path(__file__).parents[1] / "notebooks"
 
-# list of prefix paths of notebooks to skip.
 # We mainly skip because some notebooks run too long.
-SKIP_NOTEBOOKS_PREFIXES = ["notebooks/advanced_01"]
-
-
-def is_prefix(prefix: Path, path: Path) -> bool:
-    """Check if the prefix is a prefix of the path.
-
-    Parameters
-    ----------
-    prefix: Path
-        Prefix path.
-    path: Path
-        Path to check if it has the prefix.
-
-    Returns
-    -------
-    bool
-        True if the prefix is a prefix of the path, False otherwise.
-
-    """
-    # compares the parents of the paths are equal and the name of the path starts with
-    # the prefix file name
-    return all(
-        parent1 == parent2
-        for parent1, parent2 in zip(
-            prefix.resolve().parents,
-            path.resolve().parents,
-            strict=True,
-        )
-    ) and path.name.startswith(prefix.name)
+SKIP_NOTEBOOK_LIST = ["notebooks/advanced_01_hyperopt_on_bbbp.ipynb"]
 
 
 def get_notebook_paths_from_dir(notebook_dir: Path) -> list[Path]:
@@ -71,125 +42,45 @@ def get_notebook_paths_from_dir(notebook_dir: Path) -> list[Path]:
     return notebooks_paths
 
 
-def filter_notebooks(
-    notebooks_paths: list[Path],
-    skip_notebooks_prefixes: list[Path],
-) -> list[Path]:
-    """Filter out notebooks that should be skipped.
+class TestNotebooks(unittest.TestCase):
+    """Test class for testing if all Jupyter notebooks run through with error code 0."""
 
-    Parameters
-    ----------
-    notebooks_paths: list[Path]
-        List of paths to Jupyter notebooks.
-    skip_notebooks_prefixes: list[Path]
-        List of prefixes of notebooks to skip.
+    def run_notebook(
+        self,
+        notebook_path: Path,
+    ) -> None:
+        """Check all Jupyter notebooks in the given directory.
 
-    Returns
-    -------
-    list[Path]
-        Filtered list of paths to Jupyter notebooks.
+        Parameters
+        ----------
+        notebook_path: Path
+            Path to the Jupyter notebook to run.
 
-    """
-    return list(
-        filter(
-            lambda notebook_path: all(
-                not is_prefix(prefix, notebook_path)
-                for prefix in skip_notebooks_prefixes
-            ),
-            notebooks_paths,
-        ),
-    )
+        """
+        logger.info(f"Running notebook: {notebook_path}")
 
+        with notebook_path.open() as f:
+            nb = nbformat.read(f, as_version=4)
 
-def run_notebooks(
-    notebook_dir: Path,
-    skip_notebooks_prefixes: list[Path],
-    continue_on_error: bool,
-) -> None:
-    """Check all Jupyter notebooks in the given directory.
+        ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
 
-    Parameters
-    ----------
-    notebook_dir: Path
-        Path to the directory containing the Jupyter notebooks.
-    skip_notebooks_prefixes: list[Path]
-        List of prefixes of notebooks to skip.
-    continue_on_error: bool
-        If True continue running all notebooks even if an error occurs.
+        try:
+            ep.preprocess(nb, {"metadata": {"path": notebook_path.parent}})
+        except CellExecutionError as e:
+            logger.error(f"Error executing {notebook_path}.")
+            logger.error(str(e))
+            self.fail(f"Error executing {notebook_path}. Error: {e!s}")
 
-    """
-    # collect all notebook files from disc
-    notebooks_paths = get_notebook_paths_from_dir(notebook_dir)
-
-    # filter out notebooks that should be skipped
-    notebooks_paths = sorted(filter_notebooks(notebooks_paths, skip_notebooks_prefixes))
-
-    nof_errors = 0
-    # Loop through each notebook
-    for notebooks_path in notebooks_paths:
-        # Execute the notebook and capture the error code
-        cmd = [
-            "jupyter",
-            "nbconvert",
-            "--execute",
-            str(notebooks_path),
-            "--to",
-            "notebook",
-        ]
-        with subprocess.Popen(
-            cmd,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        ) as process:
-            std_out, std_err = process.communicate()
-            error_code = process.returncode
-
-        # Check if the error code is not 0
-        if error_code != 0:
-            nof_errors += 1
-            log_error_msg = (
-                f"Error executing {notebooks_path}. Error code: {error_code}"
-            )
-            logger.error(log_error_msg)
-            logger.error(std_out.decode("utf-8"))
-            logger.error(std_err.decode("utf-8"))
-
-            if not continue_on_error:
-                sys.exit(1)
-
-    # Check if there were any errors
-    if nof_errors == 0:
-        logger.success("All Jupyter notebooks executed successfully with error code 0.")
-    else:
-        logger.error(f"Jupyter notebooks executed with {nof_errors} errors.")
-        sys.exit(1)
-
-
-def main() -> None:
-    """Run the Jupyter notebooks and check if they execute without error."""
-    parser = argparse.ArgumentParser(
-        description="Test if all Jupyter notebooks run through with error code 0",
-    )
-    parser.add_argument(
-        "-c",
-        "--continue-on-failure",
-        dest="continue_on_error",
-        action="store_true",
-        help="Continue running all notebooks even if an error occurs",
-    )
-    parser.set_defaults(continue_on_error=False)
-    args = parser.parse_args()
-
-    skip_notebook_prefixes_paths = [Path(prefix) for prefix in SKIP_NOTEBOOKS_PREFIXES]
-    base_path = Path(__file__).parents[1].resolve()
-
-    for notebook_dir in NOTEBOOK_DIRS:
-        run_notebooks(
-            base_path / notebook_dir,
-            skip_notebook_prefixes_paths,
-            args.continue_on_error,
-        )
+    def test_notebooks(self) -> None:
+        """Test if all Jupyter notebooks run through with error code 0."""
+        notebooks_paths_list = get_notebook_paths_from_dir(NOTEBOOK_DIR)
+        for notebooks_path in notebooks_paths_list:
+            if str(notebooks_path) in SKIP_NOTEBOOK_LIST:
+                logger.warning(f"Skipping notebook: {notebooks_path}")
+                continue
+            with self.subTest(notebook=notebooks_path):
+                self.run_notebook(notebooks_path)
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
