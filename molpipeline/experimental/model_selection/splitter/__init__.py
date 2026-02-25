fee1a587ded837a994a3313fd9871078409c0089
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2026-02-25 17:24:19 +0100

    Remove non-existing splitters

--- molpipeline/experimental/model_selection/splitter/__init__.py
+++ molpipeline/experimental/model_selection/splitter/__init__.py
@@ -1,8 +1,5 @@
 """Initialize module for data splitting."""
 
-from molpipeline.experimental.model_selection.splitter.group_addition_splitter import (
-    GroupAdditionSplit,
-)
 from molpipeline.experimental.model_selection.splitter.group_shuffle_splitter import (
     GroupShuffleSplit,
     SplitModeOption,
@@ -10,14 +7,9 @@ from molpipeline.experimental.model_selection.splitter.group_shuffle_splitter im
 from molpipeline.experimental.model_selection.splitter.stratified_regression import (
     create_continuous_stratified_folds,
 )
-from molpipeline.experimental.model_selection.splitter.time_threshold_splitter import (
-    TimeThresholdSplitter,
-)
 
 __all__ = [
-    "GroupAdditionSplit",
     "GroupShuffleSplit",
     "SplitModeOption",
-    "TimeThresholdSplitter",
     "create_continuous_stratified_folds",
 ]
