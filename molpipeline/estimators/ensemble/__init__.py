88d244ef6e040306711fa3327fec174bedd0040e
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2026-02-27 17:13:45 +0100

    Add to init

--- molpipeline/estimators/ensemble/__init__.py
+++ molpipeline/estimators/ensemble/__init__.py
@@ -1,1 +1,8 @@
 """Module for ensemble models."""
+
+from molpipeline.estimators.ensemble.split_ensemble import (
+    SplitEnsembleClassifier,
+    SplitEnsembleRegressor,
+)
+
+__all__ = ["SplitEnsembleClassifier", "SplitEnsembleRegressor"]
