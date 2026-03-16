0ae3a321614647eb35cd2aa1158fad3c9c27936c
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2026-03-16 13:21:10 +0100

    Implement the DataRepetitionSplit

---
+++ molpipeline/experimental/model_selection/splitter/data_repetition_splitter.py
@@ -0,0 +1,89 @@
+"""The no test set splitter returns only the training set indices.
+
+This is not meant to be used for model validation but for ensembling purposes.
+
+"""
+
+from collections.abc import Iterator
+
+import numpy as np
+import numpy.typing as npt
+from sklearn.model_selection import BaseCrossValidator
+from typing_extensions import override
+
+
+class DataRepetitionSplit(BaseCrossValidator):
+    """The DataRepetitionSplit returns only the training set indices.
+
+    This is not meant to be used for model validation but for ensembling purposes.
+
+    """
+
+    def __init__(self, n_splits: int) -> None:
+        """Initialize the DataRepetitionSplit.
+
+        Parameters
+        ----------
+        n_splits : int
+            Number of splits to create.
+
+        """
+        self.n_splits = n_splits
+
+    @override
+    def split(
+        self,
+        X: npt.ArrayLike,
+        y: npt.ArrayLike | None = None,
+        groups: npt.ArrayLike | None = None,
+    ) -> Iterator[tuple[npt.NDArray[np.int64], npt.NDArray[np.int64]]]:
+        """Get the bootstrap split.
+
+        Parameters
+        ----------
+        X : array-like
+            The input data.
+        y : array-like, optional
+            The target values, by default None.
+        groups : array-like, optional
+            The group labels for the samples used while splitting the dataset into
+            train/test set. Default is None.
+
+        Yields
+        ------
+        tuple[npt.NDArray[np.int64], npt.NDArray[np.int64]]
+            The training indices and test indices for each split.
+
+        """
+        n_samples = len(X)
+        for _ in range(self.n_splits):
+            train_indices = np.arange(n_samples)
+            test_indices = np.array([], dtype=np.int64)
+            yield train_indices, test_indices
+
+    @override
+    def get_n_splits(  # type: ignore
+        self,
+        X: npt.ArrayLike,
+        y: npt.ArrayLike | None = None,
+        groups: npt.ArrayLike | None = None,
+    ) -> int:
+        """Get the number of splits.
+
+        Parameters
+        ----------
+        X : array-like
+            The input data.
+        y : array-like, optional
+            The target values, by default None.
+        groups : array-like, optional
+            The group labels for the samples used while splitting the dataset into
+            train/test set. Default is None.
+
+        Returns
+        -------
+        int
+            The number of splits.
+
+        """
+        return self.n_splits
