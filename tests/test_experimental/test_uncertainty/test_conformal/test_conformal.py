56149a6a850c15a17047ceaef3fda396c6ddb4f0
soulios-basf <kyriakos.soulios@basf.com>
2026-03-23 14:50:00 +0100

    remove global plr0914

--- tests/test_experimental/test_uncertainty/test_conformal/test_conformal.py
+++ tests/test_experimental/test_uncertainty/test_conformal/test_conformal.py
@@ -41,7 +41,7 @@ class BaseConformalTestData(unittest.TestCase):
     smiles_reg: list[str]
 
     @classmethod
-    def setUpClass(cls) -> None:  # pylint: disable=too-many-locals
+    def setUpClass(cls) -> None:
         """Set up test data once for all tests.
 
         Provides both fingerprint matrices and SMILES lists for comprehensive testing.
@@ -220,7 +220,7 @@ class TestConformalClassifier(BaseConformalTestData):
         self.assertGreaterEqual(results["ks_test"], 0.0)
         self.assertLessEqual(results["ks_test"], 1.0)
 
-    def test_evaluate_methods_cross_conformal(self) -> None:  # pylint: disable=too-many-locals
+    def test_evaluate_methods_cross_conformal(self) -> None:
         """Test evaluate methods for cross-conformal predictors (classification)."""
         x_train_fit, x_calib, x_test_clf, y_train_fit, y_calib, y_test_clf = (
             self._get_train_calib_test_splits(self.x_clf, self.y_clf)
@@ -268,7 +268,7 @@ class TestConformalClassifier(BaseConformalTestData):
         self.assertEqual(len(sets), len(y_test))
         self.assertEqual(len(p_values), len(y_test))
 
-    def test_confidence_level_effect_classification(self) -> None:  # pylint: disable=too-many-locals  # noqa: PLR0914
+    def test_confidence_level_effect_classification(self) -> None:  # noqa: PLR0914
         """Test effect of confidence level on prediction sets in ConformalClassifier."""
         x_train, x_calib, x_test, y_train, y_calib, y_test = (
             self._get_train_calib_test_splits(self.x_clf, self.y_clf)
@@ -328,7 +328,7 @@ class TestConformalClassifier(BaseConformalTestData):
         self.assertEqual(len(sets), len(y_test))
         self.assertEqual(len(p_values), len(y_test))
 
-    def test_nonconformity_functions(self) -> None:  # pylint: disable=too-many-locals
+    def test_nonconformity_functions(self) -> None:
         """Test different nonconformity functions in ConformalClassifier."""
         data_splits = self._get_train_calib_test_splits(self.x_clf, self.y_clf)
         x_train, x_calib, x_test, y_train, y_calib, y_test = data_splits
@@ -396,7 +396,7 @@ class TestConformalClassifier(BaseConformalTestData):
         self.assertTrue(np.all(np.isfinite(scores_small)))
         self.assertTrue(np.all(scores_small >= -np.log(1.0)))
 
-    def test_log_nonconformity_class(self) -> None:  # pylint: disable=too-many-locals
+    def test_log_nonconformity_class(self) -> None:
         """Test LogNonconformity functor class."""
         x_train, x_calib, x_test, y_train, y_calib, _y_test = (
             self._get_train_calib_test_splits(self.x_clf, self.y_clf)
@@ -434,7 +434,7 @@ class TestConformalClassifier(BaseConformalTestData):
         sets = cp.predict_set(x_test)
         self.assertEqual(len(sets), len(probs_test))
 
-    def test_svm_margin_binary_classification(self) -> None:  # pylint: disable=too-many-locals  # noqa: PLR0914
+    def test_svm_margin_binary_classification(self) -> None:  # noqa: PLR0914
         """Test SVMMarginNonconformity with binary SVM classification."""
         x_train, x_calib, x_test, y_train, y_calib, _y_test = (
             self._get_train_calib_test_splits(self.x_clf, self.y_clf)
@@ -587,7 +587,7 @@ class TestConformalClassifier(BaseConformalTestData):
 
         self.assertLessEqual(size_80, size_95)
 
-    def test_pipeline_wrapped_by_conformal_classifier(self) -> None:  # pylint: disable=too-many-locals  # noqa: PLR0914
+    def test_pipeline_wrapped_by_conformal_classifier(self) -> None:  # noqa: PLR0914
         """Test a MolPipeline wrapped by ConformalClassifier."""
         smi2mol = SmilesToMol()
         mol2morgan = MolToMorganFP(radius=FP_RADIUS, n_bits=FP_SIZE, return_as="dense")
@@ -718,7 +718,7 @@ class TestConformalClassifier(BaseConformalTestData):
 
         self.assertGreater(avg_prob_high_class0, avg_prob_low_class0)
 
-    def test_cross_conformal_classifier_antitonic(self) -> None:  # pylint: disable=too-many-locals
+    def test_cross_conformal_classifier_antitonic(self) -> None:
         """Test basic functionality of antitonic calibration for CCC."""
         x_train_fit, x_calib, x_test, y_train_fit, y_calib, y_test = (
             self._get_train_calib_test_splits(self.x_clf, self.y_clf)
