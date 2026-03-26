42e76525d302efb9a896e1a8a994a1df17533fd5
Kyriakos <kyriakos.soulios@basf.com>
2026-03-26 11:29:00 +0100

    Srestructuring uncertainty (#269)
    * split conformal and relevant test to 2 files due to size. removed deprecated create_continuous_stratified_spli function

---
+++ tests/test_experimental/test_uncertainty/test_conformal/__init__.py
@@ -0,0 +1,10 @@
+"""Tests for conformal prediction."""
+
+from tests.test_experimental.test_uncertainty.test_conformal.test_conformal import (
+    FP_RADIUS,
+    FP_SIZE,
+    RANDOM_SEED,
+    BaseConformalTestData,
+)
+
+__all__ = ["FP_RADIUS", "FP_SIZE", "RANDOM_SEED", "BaseConformalTestData"]
