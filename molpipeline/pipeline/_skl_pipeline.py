15b9b409f7e446cd57054ec6dccd348749051b97
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2026-05-26 15:41:09 +0200

    readd ignores

--- molpipeline/pipeline/_skl_pipeline.py
+++ molpipeline/pipeline/_skl_pipeline.py
@@ -1025,7 +1025,7 @@ class Pipeline(_Pipeline):
             # tuples and `fit` is not called yet to validate the steps.
             pass
 
-        try:
+        try:  # noqa: PLW0717
             # Only the _final_estimator is changed from the original implementation is
             # changed in the following 2 lines
             if (
