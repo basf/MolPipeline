7f5b221bc63908ba782b860b1bf8773a5a585f5b
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2026-05-27 16:57:27 +0200

    fix linebreak

--- molpipeline/experimental/explainability/visualization/heatmaps.py
+++ molpipeline/experimental/explainability/visualization/heatmaps.py
@@ -183,9 +183,7 @@ class ValueGrid(Grid2D):
         y_res: int
             Resolution (number of cells) along y-axis.
         function_list: list[\
-         Callable[\
-            [npt.NDArray[np.float64]],\
-            npt.NDArray[np.float64]],\
+            Callable[[npt.NDArray[np.float64]], npt.NDArray[np.float64]],\
         ], optional
             List of functions to be evaluated for each cell, by default None.
 
